223033e00fc290d5d33d8441656f837667a19ced
Manon Maillard <ravonyxa@gmail.com>
2017-01-25 15:38:18 +0100

    mix old and new shader

--- PostProcess/render.fs
+++ PostProcess/render.fs
@@ -1,36 +1,36 @@
 #version 130
 
-in vec2 v_textCoords;
+out vec4 color;
+
+in vec2 textCoords;
 in vec3 normals;
+in vec3 lightVector;
+in vec3 viewVector;
 
-uniform vec3 diffuse;
-uniform vec3 ambient;
-uniform vec3 specular;
-uniform vec3 camPos;
-uniform float shininess;
-
-uniform sampler2D texDiffuse;
-
-uniform int material;
-uniform int objOnly;
-uniform int useTexDiffuse;
-
-uniform int useNormals;
-uniform float light_multiplier;
-uniform vec3 light_dir;
-
-vec3 normalCalc;
-out vec4 outColor;
+uniform vec3 diffuse_albedo = vec3(0.8, 0.8, 0.9);
+uniform vec3 specular_albedo = vec3(0.01);
+uniform float specular_power = 128.0;
+uniform float shading_level = 1.0;
 
 void main()
 {
-	vec4 texD = texture2D(texDiffuse, v_textCoords);
-	normalCalc = normalize(normals);
+	color = vec4(vec3(normals), 1);
 
-		
-	outColor = vec4(vec3(normals), 1);
-	//if(useTexDiffuse == 1)
-	//	outColor = (Iambient + Idiff + Ispec) * texD;
-	//else
-	//	outColor = Iambient + Idiff + Ispec;
+	vec3 normalCalc = normalize(normals);
+    vec3 lightVectorCalc = normalize(lightVector);
+    vec3 viewVectorCalc = normalize(viewVector);
+
+    // Calculate R locally
+    vec3 R = reflect(-lightVectorCalc, normalCalc);
+
+    // Compute the diffuse and specular components for each fragment
+    vec3 diffuse = max(dot(normalCalc, lightVectorCalc), 0.0) * diffuse_albedo;
+    diffuse *= diffuse;
+    vec3 specular = pow(max(dot(R, viewVectorCalc), 0.0), specular_power) * specular_albedo;
+
+    // Write final color to the framebuffer
+    //color = mix(vec4(0.0), vec4(diffuse + specular, 1.0), shading_level);
+
+	color = vec4(vec3(normals), 1.0);
+   // normal_depth = vec4(normalCalc, viewVector.z);
 }
--- PostProcess/render.vs
+++ PostProcess/render.vs
@@ -7,15 +7,32 @@ in vec3 normal;
 uniform mat4 viewproj_matrix;
 uniform mat4 model_matrix;
 
-out vec2 v_textCoords;
+out vec2 textCoords;
 out vec3 normals;
+out vec3 lightVector;
+out vec3 viewVector;
 
+uniform vec3 light_pos = vec3(100.0, 100.0, 100.0);
 void main()
 {
-	v_textCoords = textCoord;
 	mat3 normalMatrix = mat3(model_matrix);
 	normalMatrix = inverse(normalMatrix);
 	normalMatrix = transpose(normalMatrix);
+
 	normals = normalMatrix * normal;
+	textCoords = textCoord;
+
+	/*// Calculate view-space coordinate
+    vec4 P = viewproj_matrix * position;
+
+    // Calculate normal in view-space
+    //vs_out.N = mat3(viewproj_matrix) * normal;
+
+    // Calculate light vector
+    lightVector = light_pos - P.xyz;
+
+    // Calculate view vector
+    viewVector = -P.xyz;*/
+
     gl_Position = viewproj_matrix * model_matrix * vec4(position, 1.0f) ; 
 }
